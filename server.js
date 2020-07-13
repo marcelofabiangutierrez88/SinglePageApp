e412a87104d9690441b7e267bfc8ff642acb0854
marcelo <equipostocker2020@gmail.com>
2020-07-13 20:34:49 -0300

    push heroku1

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/spa'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/index.html'));
+        '/dist/spa/index.html'));
 });
-console.log("Escuchando");
+console.log("Escuchando ");
